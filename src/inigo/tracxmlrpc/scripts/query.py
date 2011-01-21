fc82cc317b59eee7b4de8fafea8109f80853f9be
Izhar Firdaus <izhar@inigo-tech.com>
2011-01-21 07:39:25 +0000

    inigo.tracxmlrpc:  * ticket-query without parameter now queries startifact, clkss, and nordicbet

--- src/inigo/tracxmlrpc/scripts/query.py
+++ src/inigo/tracxmlrpc/scripts/query.py
@@ -1,7 +1,19 @@
 from inigo.tracxmlrpc.rpc import TracConfig, TracTicketXMLRPC
-from inigo.tracxmlrpc.scripts.common import get_parser, indent, colored
+from inigo.tracxmlrpc.scripts.common import indent, colored
 import getpass
+from argparse import ArgumentParser
 BASE="https://dev.inigo-tech.com/trac/%(project)s"
+
+ALL_TRACS=['startifact', 'clkss', 'nordicbet']
+
+def get_parser():
+    parser = ArgumentParser()
+    parser.add_argument('project', default=None, nargs='?', metavar='project', help='Project Name')
+    parser.add_argument('-u', '--username', dest='username',
+                help='Your trac login username')
+    parser.add_argument('-p', '--password', dest='password',
+                help='Your trac login password')
+    return parser
 
 def main():
     parser = get_parser()
@@ -20,15 +32,64 @@ def main():
     else:
         passwd = getpass.getpass('Password: ')
 
-    if args.ticket_id is None:
+    if args.project is None:
+        render_all(user, passwd, args, ALL_TRACS)
+    elif args.ticket_id is None:
         render_list(user, passwd, args)
     else:
         render_view(user, passwd, args)
 
+def sort_tickets(tickets):
+    tickets.sort(key=lambda x: x.id, reverse=True)
+    def sortfunc(a, b):
+        if a.priority.lower() == 'critical':
+            return 1
+        if a.priority.lower() == 'major' and b.priority.lower() == 'minor':
+            return 1
+        return -1
+    tickets.sort(cmp=sortfunc)
+    return tickets
+
 def render_list(user, passwd, args):
+    tickets = sort_tickets(ticket_list(user, passwd, args))
+
+    for ticket in tickets:
+        color = 'default'
+        bold = False
+        if ticket.priority.lower() == 'critical':
+            color = 'red'
+            bold = True
+        print colored('#%-5d| %s' % (ticket.id, ticket.summary), color, bold)
+
+
+def render_all(user, passwd, args, tracs):
+    all_tickets = []
+    for trac in tracs:
+        args.project = trac
+        tickets = ticket_list(user, passwd, args)
+        for ticket in tickets:
+            ticket.project = trac
+        all_tickets += tickets
+
+    all_tickets = sort_tickets(all_tickets)
+
+    for ticket in all_tickets:
+        color = 'default'
+        bold = False
+        if ticket.priority.lower() == 'critical':
+            color = 'red'
+            bold = True
+        elif ticket.priority.lower() == 'major':
+            color = 'yellow'
+        print colored('%s|#%-5d| %s' % (ticket.project.ljust(13), ticket.id, ticket.summary), color, bold)
+
+
+def ticket_list(user, passwd, args):
+
+    result = []
+
     conf = TracConfig(BASE % dict(project=args.project), 
                     user, passwd)
-
     ticketrpc = TracTicketXMLRPC(conf)
 
     if args.filter_owner:
@@ -39,14 +100,7 @@ def render_list(user, passwd, args):
     else:
         tickets = ticketrpc.query()
 
-    for ticket in sorted(tickets, key=lambda x: x.id, reverse=True):
-        color = 'default'
-        bold = False
-        if ticket.priority.lower() == 'critical':
-            color = 'red'
-            bold = True
-        print colored('#%s\t| %s' % (ticket.id, ticket.summary), color, bold)
-
+    return tickets
 
 def render_view(user, passwd, args):
     conf = TracConfig(BASE % dict(project=args.project),
